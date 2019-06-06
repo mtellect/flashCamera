036d582af3cfcacc33a3b62412679be63f6d1fe1
bappstack <ammaugost@gmail.com>
2019-06-06 12:25:50 +0100

    renamed repo to flash camera

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:camera/camera.dart';
+import 'package:flashCamera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
